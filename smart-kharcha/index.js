fbf73edc964ac6c7fafa8506792df8aae2c8bc31
Utkarsh Raj <Utkarshr571@gmail.com>
2026-02-07 00:09:04 +0530

    routing

--- smart-kharcha/index.js
+++ smart-kharcha/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+const method_override = require("method-override");
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use(method_override("_method"));
+
 app.get("/",(req,res) =>{
-    res.send("this is my main page");
+    //res.send("this is my main page");
+    res.render("home.ejs");
 })
 
 app.get("/home",(req,res)=>{
-    res.send("hello world");
+    res.render("dashboard.ejs");
 })
 
 app.listen("8080",()=>{
